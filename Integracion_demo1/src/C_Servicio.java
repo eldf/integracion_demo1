b9ccad163456ad2bb1d16343b3b469b52c55dc8b
diego <diegofernd@gmail.com>
2017-08-13 16:09:41 -0500

    codigo java
    prueba1

---
+++ Integracion_demo1/src/C_Servicio.java
@@ -0,0 +1,20 @@
+
+public class C_Servicio {
+	
+	String Nombre;
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo 123");
+
+	}
+
+}
