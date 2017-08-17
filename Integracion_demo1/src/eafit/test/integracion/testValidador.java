7ffdc350b1ca351d6c89c5651d93f2dd40eac152
diego <diegofernd@gmail.com>
2017-08-16 22:28:20 -0500

    otra prueba

--- Integracion_demo1/src/eafit/test/integracion/testValidador.java
+++ Integracion_demo1/src/eafit/test/integracion/testValidador.java
@@ -2,10 +2,6 @@ package eafit.test.integracion;
 
 import java.util.Arrays;
 import java.util.Collection;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
 
 
