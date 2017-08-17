08e91c3cfe5ffaabd114db03ced071fe690c6e2e
diego <diegofernd@gmail.com>
2017-08-16 21:43:48 -0500

    se hace una copia

--- Integracion_demo1/src/eafit/integracion/C_Validar.java
+++ Integracion_demo1/src/eafit/integracion/C_Validar.java
@@ -2,8 +2,27 @@ package eafit.integracion;
 
 public class C_Validar {
 	
-	Validador val;
-	Tipo tipo;
+	String cadena;
+	
+	boolean ValidarRqUid() {
+		
+		boolean estado=true;
+		
+		if(this.cadena==null) {
+			estado=false;
+		}
+		
+		return estado;
+	}
+	
+	
+	public String getCadena() {
+		return cadena;
+	}
+
+	public void setCadena(String cadena) {
+		this.cadena = cadena;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ Integracion_demo1/src/eafit/test/integracion/testValidador.java
@@ -0,0 +1,14 @@
+package eafit.test.integracion;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+
+
+public class testValidador {
+
+}
